24bad975d9f0c39bb6b77f6d66f9933679ebf5c2
Miles Olson <mpolson64@fb.com>
2018-06-28 12:01:35 -0700

    Base layout of now playing

--- Flix2/Controllers/MovieCollectionViewController.m
+++ Flix2/Controllers/MovieCollectionViewController.m
@@ -6,12 +6,16 @@
 //  Copyright © 2018 codepath. All rights reserved.
 //
 
-#import "MovieCollectionViewController.h"
+#import "UIImageView+AFNetworking.h"
 
-@interface MovieCollectionViewController ()
+#import "MovieCollectionViewController.h"
+#import "MovieCollectionViewCell.h"
+
+@interface MovieCollectionViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 @property (strong, nonatomic) UIRefreshControl *refreshControl;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
+@property (strong, nonatomic) NSArray *movies;
 @end
 
 @implementation MovieCollectionViewController
@@ -19,13 +23,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.collectionView addSubview:self.refreshControl];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *) self.collectionView.collectionViewLayout;
+    int postersPerRow = 3;
+    
+    layout.itemSize = CGSizeMake(self.collectionView.frame.size.width / postersPerRow, (self.collectionView.frame.size.width / postersPerRow) * 1.5);
+    
+    [self fetchMovies];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)fetchMovies {
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            
+            self.movies = dataDictionary[@"results"];
+            
+            [self.collectionView reloadData];
+            NSLog(@"%@", self.movies);
+        }
+        
+        [self.refreshControl endRefreshing];
+    }];
+    
+    [task resume];
 }
 
 /*
@@ -37,5 +75,20 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionViewCell" forIndexPath:indexPath];
+    
+    NSDictionary *movie = self.movies[indexPath.row];
+    cell.posterImageView.image = nil;
+    [cell.posterImageView setImageWithURL:[NSURL URLWithString:[@"https://image.tmdb.org/t/p/w500" stringByAppendingString:movie[@"poster_path"]]]];
+
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+
 
 @end
--- Flix2/Views/MovieCollectionViewCell.h
+++ Flix2/Views/MovieCollectionViewCell.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface MovieCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *posterImageView;
 
 @end
